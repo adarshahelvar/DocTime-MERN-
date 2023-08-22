219b0d0b71287b986bff4927ec68617c29966675
Adarsha A <adarshaadi1997@gmail.com>
2023-08-22 14:31:45 +0530

    Basic frontend routing

---
+++ frontend/src/pages/Doctors/DoctorDetailes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DoctorDetailes = () => {
+  return (
+    <div>DoctorDetailes</div>
+  )
+}
+
+export default DoctorDetailes
---
+++ frontend/src/routes/Routers.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from '../pages/Home';
+import Services from '../pages/Services';
+import Login from '../pages/Login';
+import  Signup from '../pages/Signup';
+import  Contact from '../pages/Contact';
+import Doctors from '../pages/Doctors/Doctors';
+import DoctorDetailes from "../pages/Doctors/DoctorDetailes";
+
+const Routers = () => {
+  return (
+    <div>
+      <Routes>
+      <Route path="/" element={<Home />} /> 
+      <Route path="/home" element={<Home />} /> 
+      <Route path="/services" element={<Services />} /> 
+      <Route path="/login" element={<Login />} /> 
+      <Route path="/signup" element={<Signup />} /> 
+      <Route path="/contact" element={<Contact />} /> 
+      <Route path="/doctors" element={<Doctors />} /> 
+      <Route path="/doctordetailes" element={<DoctorDetailes />} /> 
+      </Routes>
+    </div>
+  );
+};
+
+export default Routers;
