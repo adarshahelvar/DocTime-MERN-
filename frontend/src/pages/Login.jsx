a9d2dc37b34cc3d1e5755faf23198a885c216a61
Adarsha A <adarshaadi1997@gmail.com>
2023-08-29 12:49:36 +0530

    Register page completed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -22,8 +22,9 @@ const Login = () => {
           <div className="mb-5">
             <input
               type="email"
-              placeholder="Enter Yout Email"
+              placeholder="Enter Your Email"
               name="email"
+              value={formData.email}
               onChange={handleInputChange}
               className="w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColoor rounded-md cursor-pointer"
             />
@@ -31,8 +32,9 @@ const Login = () => {
           <div className="mb-5">
             <input
               type="password"
-              placeholder="Enter Yout password"
+              placeholder="Enter Your password"
               name="password"
+              value={formData.password}
               onChange={handleInputChange}
               className="w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColoor rounded-md cursor-pointer"
             />
@@ -46,7 +48,10 @@ const Login = () => {
             </button>
           </div>
           <p className="mt-5 text-textColor text-ceenter">
-            Don't have an account?<Link to='/register' className="text-primaryColor ">Register</Link>
+            Don't have an account?
+            <Link to="/register" className="text-primaryColor ">
+              Register
+            </Link>
           </p>
         </form>
       </div>
