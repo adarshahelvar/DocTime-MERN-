b6e4b16970ab2c350c9918aafa0e01e92e164bc5
adarshahelvar <adarshaadi1997@gmail.com>
2023-10-31 13:23:47 +0530

    login component completed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <AuthContextProvider>
+        <ToastContainer
+          theme="dark"
+          position="bottom-center"
+          autoClose={3000}
+          closeOnClick
+          pauseOnHover={false}
+        />
+        <App />
+      </AuthContextProvider>
     </React.StrictMode>
   </BrowserRouter>
 );
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,15 +1,59 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Signup from "./Signup";
+import { toast } from "react-toastify";
+import HashLoader from "react-spinners/HashLoader";
+import { BASE_URL } from "../config";
+import { authContext } from "../context/AuthContext.js";
 
 const Login = () => {
+  const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
 
+  const navigate = useNavigate();
+  const { dispatch } = useContext(authContext);
+
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const submitHandler = async (event) => {
+    // console.log(formData);
+    event.preventDefault();
+    setLoading(true);
+    try {
+      const res = await fetch(`${BASE_URL}/auth/login`, {
+        method: "POST",
+        headers: {
+          "content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const result = await res.json();
+      console.log(result)
+      if (!result.success) {
+        throw new Error(result.message);
+      }
+console.log("hello world!");
+      dispatch({
+        type: "LOGIN_SUCCESS",
+        payload: {
+          user: result.data,
+          token: result.token,
+          role: result.role,
+        },
+      });
+      console.log(result, "Login data");
+      setLoading(false);
+      toast.success(result.message);
+      navigate("/home");
+    } catch (e) {
+      toast.error(e.message);
+      setLoading(false);
+    }
   };
 
   return (
@@ -18,7 +62,7 @@ const Login = () => {
         <h3 className="text-headingColor text-[22px] leading-9 font-bold mb-10">
           Hello! <span className="text-primaryColor">Welcome</span> Back
         </h3>
-        <form action="" className="py- md:py-0">
+        <form action="" className="py- md:py-0" onSubmit={submitHandler}>
           <div className="mb-5">
             <input
               type="email"
