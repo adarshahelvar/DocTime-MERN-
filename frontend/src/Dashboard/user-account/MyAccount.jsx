cf177d5549ae47f1e9112fa9b811201bacbac3bf
adarshahelvar <adarshaadi1997@gmail.com>
2023-11-08 13:35:34 +0530

    Dynamic user name, profile pho and image getting

--- frontend/src/Dashboard/user-account/MyAccount.jsx
+++ frontend/src/Dashboard/user-account/MyAccount.jsx
@@ -16,7 +16,7 @@ const MyAccount = () => {
     loading,
     error,
   } = useGetProfile(`${BASE_URL}/users/profile/me`);
-  // console.log(userData, "userData");
+  console.log(userData, "userData");
 
   const handleLogout = () => {
     dispatch({ type: "LOGOUT" });
@@ -35,24 +35,24 @@ const MyAccount = () => {
               <div className="flex items-center justify-center">
                 <figure className="w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor">
                   <img
-                    src={userImg}
+                    src={userData.photo}
                     alt=""
-                    className="w-full f-full rounded-full"
+                    className="w-full f-full rounded-full h-full"
                   />
                 </figure>
               </div>
               <div className="text-center mt-4">
                 <h3 className="text-[18px] leading-[30px] text-headingColor font-bold ">
                   {" "}
-                  Adarsha A
+                  {userData.name}
                 </h3>
                 <p className="text-textColor text-[15px] leaidng-6 font-medium">
-                  example@gmial.com
+                  {userData.email}
                 </p>
                 <p className="text-textColor text-[15px] leaidng-6 font-medium">
                   Blood Type:{" "}
                   <span className="ml-2 text-headingColor text-[22px] leading-8">
-                    o-
+                    {userData.bloodType}
                   </span>
                 </p>
               </div>
